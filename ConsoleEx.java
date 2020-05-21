030e6bdd067bc1289078850c1728d162f8143174
rakesh Ray <hedaoo.sagar0@gmail.com>
2020-05-21 17:07:10 +0530

    update0

---
+++ ConsoleEx.java
@@ -0,0 +1,17 @@
+import java.io.Console;
+
+public class ConsoleEx {
+
+	public static void main(String[] args) {
+
+		Console c=System.console();
+		System.out.println("Enter name");
+		try{
+	
+String n=c.readLine();
+System.out.println("Welcome "+n);
+		}catch(NullPointerException e){System.out.println("caught ");}
+		
+	}
+
+}
